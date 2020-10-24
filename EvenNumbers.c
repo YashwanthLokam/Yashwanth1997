48f8752e0b8b04587c51a4420797de016338b4a5
YashwanthLokam <yashwanthlokam1997@gmail.com>
2020-10-24 17:05:34 +0530

    Update EvenNumbers.c
    change1

--- EvenNumbers.c
+++ EvenNumbers.c
@@ -2,14 +2,13 @@
 int main()
 {
 	printf("How many Even numbers do you want? ");
-	int countOfEvenNumbers;
 	scanf("%d", &countOfEvenNumbers);
-	printf("Even numbers are: ");
 	int evenNumber = 2;
 	for(int evenNumberCounter = 1; evenNumberCounter <= countOfEvenNumbers; evenNumberCounter++, printf("%d\t", evenNumber), evenNumber = evenNumber + 2);
 	{
 		printf("%d\t", evenNumber);
 		evenNumber = evenNumber + 2;
 	}
+	printf("Print all even numbers");
 	return 0;
 }
