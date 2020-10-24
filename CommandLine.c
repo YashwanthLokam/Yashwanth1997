5b5bf8295ad0cbe7044a5dfe01eac2ecaa1358b0
YashwanthLokam <yashwanthlokam1997@gmail.com>
2020-10-24 21:41:28 +0530

    Third commit

---
+++ CommandLine.c
@@ -0,0 +1,9 @@
+//Print command line arguments
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int counter;
+	for(counter=0; counter<argc; printf("argv[%d]: %s\n", counter, argv[counter]), counter++);
+	return 0;
+}
