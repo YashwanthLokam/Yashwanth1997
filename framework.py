30723521ad9943895326e111197458741624807e
YashwanthLokam <yashwanthlokam1997@gmail.com>
2020-11-03 10:15:18 +0530

    Final commit

--- framework.py
+++ framework.py
@@ -1,42 +1,4 @@
 """Framework program"""
-file_not_found = "File does not exist or unable to open the file."
-menu_file_name = "menu.cfg"
-with open(menu_file_name) as menu_file_obj:
-	try:
-		menu = menu_file_obj.read()
-	except FileNotFoundError:
-		print(file_not_found)
-	else:
-		menu_file_obj.close()
-
-updatable_fields_position_file = "updatable_fields_position.cfg"
-with open(updatable_fields_position_file) as updatable_fields_position_obj:
-	try:
-		updatable_fields_position = updatable_fields_position_obj.readlines()
-	except FileNotFoundError:
-		print(file_not_found)
-	else:
-		updatable_fields_position_obj.close()
-
-field_names_file = "fields.cfg"
-with open(field_names_file) as field_names_obj:
-	try:
-		field_names = field_names_obj.readlines()
-	except FileNotFoundError:
-		print(file_not_found)
-	else:
-		field_names_obj.close()
-
-try:
-	data_file_name = "data.dat"
-	with open(data_file_name) as data_obj:
-		records = data_obj.read()
-		records = eval(records)
-except FileNotFoundError:
-	with open(data_file_name, 'w') as data_obj:
-		records = []
-		data_obj.write(str(records))
-		data_obj.close()
 
 def print_record_not_found():
 	print("Record is not found.")
@@ -46,6 +8,11 @@ def print_record(field_values):
 	for field_name in field_names:
 		print(field_name.rstrip(), ": ", field_values[index])
 		index += 1
+
+def save_all_records():
+	with open(data_file_name, 'w') as data_obj:
+		data_obj.write(str(records))
+	data_obj.close()
 
 def create_record():
 	field_values = []
@@ -57,9 +24,7 @@ def create_record():
 		field_value = input()
 		field_values.append(field_value)
 	records.append(field_values)
-	with open(data_file_name, 'w') as data_obj:
-		data_obj.write(str(records))
-	data_obj.close()
+	save_all_records()
 	print("Record is inserted.")
 
 def print_all_records():
@@ -91,9 +56,7 @@ def update_record():
 			print("Enter ", field_names[int(updatable_fields_position[choice - 1].rstrip()) - 1].rstrip() ,": ", end = "")
 			record[int(updatable_fields_position[choice - 1].rstrip())] = input()
 			print(field_names[int(updatable_fields_position[choice - 1].rstrip()) - 1].rstrip(), "is updated.")
-	with open(data_file_name, 'w') as data_obj:
-		data_obj.write(str(records))
-	data_obj.close()
+	save_all_records()
 	if is_record_found == False:
 		print_record_not_found()
 
@@ -106,13 +69,57 @@ def delete_record():
 			is_record_found = True
 			record[0] ='D'
 			print("Record is deleted.")
-	with open(data_file_name, 'w') as data_obj:
-		data_obj.write(str(records))
-	data_obj.close()
+	save_all_records()
 	if is_record_found == False:
 		print_record_not_found()
 
-functions_list = [create_record, search_record, print_all_records, update_record, delete_record, exit]
+def confirm_to_exit():
+	print("Do you want to exit? ")
+	print("Press Y or N", ": ", end = "")
+	choice = input()
+	if choice == 'Y':
+		exit()
+
+file_not_found_message = "File does not exist or unable to open the file."
+menu_file_name = "menu.cfg"
+with open(menu_file_name) as menu_file_obj:
+	try:
+		menu = menu_file_obj.read()
+	except FileNotFoundError:
+		print(file_not_found_message)
+	else:
+		menu_file_obj.close()
+		
+try:
+	data_file_name = "data.dat"
+	with open(data_file_name) as data_obj:
+		records = data_obj.read()
+		records = eval(records)
+except FileNotFoundError:
+	with open(data_file_name, 'w') as data_obj:
+		records = []
+		data_obj.write(str(records))
+		data_obj.close()
+
+updatable_fields_position_file = "updatable_fields_position.cfg"
+with open(updatable_fields_position_file) as updatable_fields_position_obj:
+	try:
+		updatable_fields_position = updatable_fields_position_obj.readlines()
+	except FileNotFoundError:
+		print(file_not_found_message)
+	else:
+		updatable_fields_position_obj.close()
+
+field_names_file = "fields.cfg"
+with open(field_names_file) as field_names_obj:
+	try:
+		field_names = field_names_obj.readlines()
+	except FileNotFoundError:
+		print(file_not_found_message)
+	else:
+		field_names_obj.close()
+
+functions_list = [create_record, search_record, print_all_records, update_record, delete_record, confirm_to_exit]
 
 while True:
 	print(menu)
