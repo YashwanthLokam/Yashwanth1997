fdf9856b867254ff7a6a2c927ff8a144f9a1aa0e
YashwanthLokam <yashwanthlokam1997@gmail.com>
2020-11-03 15:47:39 +0530

    Final commit

--- dictionaryprog.py
+++ dictionaryprog.py
@@ -3,7 +3,7 @@ import requests
 import os
 
 user_word = input("Enter a word to find its meaning and pronunciation: ")
-URL = 'https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/' + user_word + '?strictMatch=True'
+URL = "https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/" + user_word + "?strictMatch=True"
 
 headers = {
   'Accept': 'application/json',
@@ -15,5 +15,5 @@ information_of_word = requests.get(url = URL, headers = headers).json()
 audio_file_url = information_of_word['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile']
 meaning_of_word = information_of_word['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
 print(meaning_of_word)
-command = 'vlc -I null --play-and-exit ' + audio_file_url
+command = "vlc -I null --play-and-exit " + audio_file_url
 os.system(command)
