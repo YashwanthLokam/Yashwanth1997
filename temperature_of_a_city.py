0ce1566b69e9db9bf86cedaac3d6a6875ad6f3a2
YashwanthLokam <yashwanthlokam1997@gmail.com>
2020-11-05 22:22:38 +0530

    Added speech to text function

--- temperature_of_a_city.py
+++ temperature_of_a_city.py
@@ -1,9 +1,20 @@
 """Program to print temperature of a city"""
 import requests
+import speech_recognition as sr 
 
-location = input("Enter city name: ")
-URL =  "http://api.openweathermap.org/data/2.5/find?&appid=7a9f30882737a25fea0fcf2974889d24&units=metric&q=" + location
-weather_report = requests.get(url = URL)
-weather_report = weather_report.json()
-temperature_of_a_city = weather_report['list'][0]['main']['temp']
-print("The temperature in " + location + " is " + str(temperature_of_a_city) + chr(176) + "C.")
+print("Tell your location to find temp: ")
+r = sr.Recognizer()
+try:
+	with sr.Microphone() as source:
+		audio_data = r.record(source, duration = 2)
+		location = r.recognize_google(audio_data)
+		URL =  "http://api.openweathermap.org/data/2.5/find?&appid=7a9f30882737a25fea0fcf2974889d24&units=metric&q=" + location
+		weather_report = requests.get(url = URL)
+		if weather_report.status_code == 200:
+			weather_report = weather_report.json()
+			temperature_of_a_city = weather_report['list'][0]['main']['temp']
+			print("The temperature in " + location + " is " + str(temperature_of_a_city) + chr(176) + "C.")
+		else:
+			print("PLEASE TELL A VALID CITY.")
+except:
+	print("PLEASE TELL A VALID CITY.")
