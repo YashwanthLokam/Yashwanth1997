75b919d247c2d1d51aa3de37b8c696ea675df600
YashwanthLokam <yashwanthlokam1997@gmail.com>
2020-11-17 15:19:02 +0530

    Final commit

---
+++ framework_using_SQLite.py
@@ -0,0 +1,133 @@
+"""Insurance with SQLite"""
+import sqlite3
+file_not_found_message = "File does not exist or unable to open the file."
+
+menu_file_name = "menu.cfg"
+with open(menu_file_name) as menu_file_obj:
+	try:
+		menu = menu_file_obj.read()
+	except FileNotFoundError:
+		print(file_not_found_message)
+	else:
+		menu_file_obj.close()
+
+updatable_fields_position_file = "updatable_fields_position.cfg"
+with open(updatable_fields_position_file) as updatable_fields_position_obj:
+	try:
+		updatable_fields_position = updatable_fields_position_obj.readlines()
+	except FileNotFoundError:
+		print(file_not_found_message)
+	else:
+		updatable_fields_position_obj.close()
+
+table_names_file = "table_names.cfg"
+with open(table_names_file) as table_names_obj:
+	try:
+		table_names = table_names_obj.read()
+		table_names = eval(table_names)
+	except FileNotFoundError:
+		print(file_not_found_message)
+	else:
+		table_names_obj.close()
+
+connection = sqlite3.connect("framework.db")
+cursor = connection.execute("pragma table_info(" + '\'' + table_names[0] + '\'' + ")")
+field_names = []
+for field_name in cursor:
+	if field_name[1] != 'Status':
+		field_names.append(field_name[1])
+
+def check_record_present(id):
+	is_record_found = False
+	records = connection.execute('SELECT ' + field_names[0] + ' FROM ' + table_names[0] + ' WHERE Status = "A"')
+	for record in records:
+		if record[0] == id:
+			is_record_found = True
+	return is_record_found
+
+def print_record(record):
+	index = 0
+	for field_name in field_names:
+		print(field_name + ": " + str(record[index]))
+		index += 1
+	print('-' * 20)
+
+def print_record_not_found():
+	print("Record is not found in the table.")
+
+def get_id():
+	return input("Enter " + field_names[0] + ": ")
+
+def create_record():
+	field_values = []
+	for field_name in field_names:
+		print(field_name + ": ", end = "")
+		field_value = input()
+		field_values.append(field_value)
+	field_values.append('A')
+	record = tuple(field_values)
+	connection.execute('INSERT INTO '+ table_names[0] + ' VALUES' + str(record))
+	connection.commit()
+	print("Record is inserted.")
+
+def search_record():
+	id_to_search_record = get_id()
+	is_record_found = check_record_present(id_to_search_record)
+	if is_record_found == True:
+		record_obj = connection.execute('SELECT * FROM '+ table_names[0] + ' WHERE ' + field_names[0] + ' = ' + id_to_search_record)
+		record = record_obj.fetchone()
+		print_record(record)
+	else:
+		print_record_not_found()
+
+def print_all_records():
+	query = "SELECT * FROM " + table_names[0] + " WHERE Status = 'A'"
+	records = connection.execute(query)
+	for record in records:
+		print_record(record)
+
+def delete_record():
+	id_to_delete_record = get_id()
+	is_record_found = check_record_present(id_to_delete_record)
+	if is_record_found == True: 
+		connection.execute('UPDATE ' + table_names[0] + ' SET Status = "D" WHERE ' + field_names[0] + ' = ' + id_to_delete_record)
+		connection.commit()
+		print("Record is deleted.")
+	else:
+		print_record_not_found()
+		
+def update_record():
+	id_to_update_record = get_id()
+	is_record_found = check_record_present(id_to_update_record)
+	if is_record_found == True:
+		counter = 1
+		for field_position in updatable_fields_position:
+			print(str(counter) + ". Update " + field_names[int(field_position.rstrip()) - 1])
+			counter += 1
+		choice = int(input("Enter a number: "))
+		print("Enter " + field_names[int(updatable_fields_position[choice - 1].rstrip()) - 1] +": ", end = "")
+		field_value_to_update_record = input()
+		connection.execute('UPDATE ' + table_names[0] + ' SET ' + field_names[int(updatable_fields_position[choice - 1].rstrip()) - 1] + ' = ' + "\"" + field_value_to_update_record + "\""+ ' WHERE ' + field_names[0] + ' = ' + id_to_update_record)
+		connection.commit()
+		print(field_names[int(updatable_fields_position[choice - 1].rstrip()) - 1] + " is updated.")
+	else:
+		print_record_not_found()
+
+def confirm_to_exit():
+	print("Do you want to exit? ")
+	print("Press Y or N" + ": ", end = "")
+	choice = input()
+	if choice.upper() == 'Y':
+		connection.close()
+		exit()
+
+functions_list = [create_record, search_record, print_all_records, update_record, delete_record, confirm_to_exit]
+
+while True:
+	print(menu)
+	user_choice = input("Enter a number: ")
+	user_choice = int(user_choice)
+	if user_choice > 0 and user_choice <= 6:
+		functions_list[user_choice - 1]()
+	else:
+		print("ENTER A VALID NUMBER.")
